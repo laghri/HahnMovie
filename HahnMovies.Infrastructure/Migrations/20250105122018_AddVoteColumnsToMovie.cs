673916cdd48d393e03bd074b723e00250a48ad8e
AhmedLaghrissi <ahmedlaghrissi10@gmail.com>
2025-01-05 13:45:34 +0100

    add migrations

---
+++ HahnMovies.Infrastructure/Migrations/20250105122018_AddVoteColumnsToMovie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HahnMovies.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVoteColumnsToMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    VoteAverage = table.Column<double>(type: "float", nullable: false),
+                    PosterPath = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HahnVoteCount = table.Column<int>(type: "int", nullable: false),
+                    HahnVoteAverage = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ HahnMovies.Infrastructure/Migrations/20250105123926_AddMovieCommentsTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HahnMovies.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovieCommentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MovieComments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieComments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MovieComments_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieComments_MovieId",
+                table: "MovieComments",
+                column: "MovieId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieComments");
+        }
+    }
+}
